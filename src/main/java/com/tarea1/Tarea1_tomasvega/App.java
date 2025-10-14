61aa53b28ec5d6e3d38191e4723b22b3128ea1ad
2damv25 <2damv25@A307PC19.desarrolloweb.local>
2025-10-14 16:32:30 +0200

    version estable del App.java

--- src/main/java/com/tarea1/Tarea1_tomasvega/App.java
+++ src/main/java/com/tarea1/Tarea1_tomasvega/App.java
@@ -2,23 +2,44 @@ package com.tarea1.Tarea1_tomasvega;
 
 import entidades.Perfil;
 import entidades.Sesion;
+import entidades.Perfil;
+import entidades.Sesion;
 
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-    	Sesion nueva_sesion = new Sesion("Invitado", Perfil.INVITADO);
-    	
+    	Sesion nuevaSesion = new Sesion("Invitado", Perfil.INVITADO);
+    	boolean sesionIniciada = true;
     	do {
-    		System.out.println("============================================");
-    		System.out.println(" ");
+    		System.out.println("============================================\n");
     		System.out.println("	Programa de gestion de circo");
     		System.out.println(" ");
     		System.out.println(" ");
     		System.out.println("	Por Tomas Jesus Vega Leiva");
     		System.out.println(" ");
     		System.out.println("============================================");
-		} while (condition);
+    		showGuestMenu();
+    		sesionIniciada = false;
+		} while (sesionIniciada == true);
+    }
+    
+    public static void nuevaSeleccion (int opcion) {
+    	
+    }
+    
+    public static void showGuestMenu () {
+    	System.out.println("Sesion: ");
+    	System.out.println("1. Login");
+    	System.out.println("2. Ver espectaculos");
+    }
+    
+    public static void showCoordMenu () {
+    	
+    }
+    
+    public static void showAdminMenu () {
+    	
     }
 }
